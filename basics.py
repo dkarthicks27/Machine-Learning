5562c6ce02878a76917fa97074739c95ed87b33a
dkarthicks27 <dkarthicks27@gmail.com>
2020-04-06 23:25:33 +0530

    File changed

--- basics.py
+++ basics.py
@@ -1,1 +1,23 @@
-print("this is our first project")
+# dictionary - school attendance record
+# names, id, arrange them alphabetic order
+# according to their name
+
+# according to their id
+attendance = {'Karthick': 12345, 'Jyo': 21345, 'Sashank': 12435}
+
+
+def arrangeWithName():
+    names = []
+    for key in attendance:
+        names.append(key)
+    names.sort()
+    for i in names:
+        print(i, attendance[i])
+
+
+def arrangeWithId():
+    pass
+
+
+arrangeWithName()
+arrangeWithId()
