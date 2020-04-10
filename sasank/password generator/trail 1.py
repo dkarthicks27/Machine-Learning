d65e5abe944812c9e96eb02098dfe8ea1c6116df
sasank9999 <63254550+sasank9999@users.noreply.github.com>
2020-04-10 22:23:05 +0530

    Update trail 1.py

--- sasank/password generator/trail 1.py
+++ sasank/password generator/trail 1.py
@@ -1,1 +1,8 @@
-vcs
+# generate a random password with length "of  MIN 6 charcters" 
+import random STRING
+def pw_gen( chars= STRING Upper & Lower case + string.digits + string.SYMBOLS):
+ ENTER THE REQUIREMENT TO DISPLAY PASSWORD
+    IF PASSWORD LENGTH >=6
+       "PRINT PASSWORD"
+    ELSE ;
+       PRINT "PASSWORD INSUFFICENT"
