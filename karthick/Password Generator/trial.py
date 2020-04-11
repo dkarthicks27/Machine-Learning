92579d767fac67237abb55ff0a27908b49204422
dkarthicks27 <dkarthicks27@gmail.com>
2020-04-11 14:14:02 +0530

    changed files

--- karthick/Password Generator/trial.py
+++ karthick/Password Generator/trial.py
@@ -1,5 +1,5 @@
 from random import sample
-from random import randint
+from random import randint, shuffle
 from random import seed
 import string
 
@@ -20,25 +20,26 @@ def passwordGenerator(uppercase=1, lowercase=2, numbers=3, symbols=4, reps=20):
         subset[pos:pos] = list(sample(l2, lowercase))
         subset[pos2:pos2] = list(sample(l3, numbers))
         subset[pos3:pos3] = list(sample(l4, symbols))
+        shuffle(subset)
         password = ''.join(map(str, subset))
         print("\nThe generated Password is: " + password)
         i = i+1
 
 
 if __name__ == '__main__':
-    print("this is a program to generate a random password")
+    print("this is a program to generate a random password\tThere can be 18 char max-")
     upperCase = int(input("\nEnter the no. of upper case letters: "))
-    if upperCase > 12:
-        upperCase = 12
+    if upperCase > 5:
+        upperCase = 4
     lowerCase = int(input("Enter the no. of lower case letters: "))
-    if lowerCase > 12:
-        lowerCase = 12
+    if lowerCase > 5:
+        lowerCase = 4
     nNumbers = int(input("Enter the no. of numbers you want to use: "))
-    if nNumbers > 10:
-        nNumbers = 10
+    if nNumbers > 4:
+        nNumbers = 4
     nSymbols = int(input("Enter the no. of symbols you want to use: "))
-    if nSymbols > 20:
-        nSymbols = 20
+    if nSymbols > 4:
+        nSymbols = 4
     iterations = int(input("no. of passwords you want to generate: "))
     if iterations > 20:
         iterations = 20
--- sasank/password generator/trail 1.py
+++ sasank/password generator/trail 1.py
@@ -1,8 +1,22 @@
 # generate a random password with length "of  MIN 6 charcters" 
-import random STRING
-def pw_gen( chars= STRING Upper & Lower case + string.digits + string.SYMBOLS):
- ENTER THE REQUIREMENT TO DISPLAY PASSWORD
-    IF PASSWORD LENGTH >=6
-       "PRINT PASSWORD"
-    ELSE ;
-       PRINT "PASSWORD INSUFFICENT"
+from random import seed
+from random import randint
+import string
+
+
+def pw_gen(password):
+    # ENTER THE REQUIREMENT TO DISPLAY PASSWORD
+    # "saShank23$"
+    l1 = set(string.ascii_uppercase)
+    l2 = set(string.ascii_lowercase)
+    l3 = set(string.digits)
+    l4 = set(string.punctuation)
+    pwd = set([ch for ch in str(password)])
+    if len(pwd) > 6 and len(pwd.intersection(l1)) > 0 and len(pwd.intersection(l2)) > 0 and len(pwd.intersection(l3)) > 0 and len(pwd.intersection(l4)) > 0:
+        print("your password ", password, " is approved")
+    else:
+        print("PASSWORD INSUFFICENT")
+
+
+x = input("pls enter your password: ")
+pw_gen(x)
